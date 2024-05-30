9a64e0721ba642d26d35678adc27fb0ca57a0dee
timosully <168386243+timosully@users.noreply.github.com>
2024-05-30 01:17:23 +0000

    Added routing to error's 404 and 500 in the views file, then created html files for 404 and 500.

--- Assignment3/website/views.py
+++ Assignment3/website/views.py
@@ -9,3 +9,12 @@ def index():
 @main_bp.route('/create_event')
 def create_event():
     return render_template('eventCreate.html')
+
+# Error handlers
+@main_bp.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@main_bp.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
